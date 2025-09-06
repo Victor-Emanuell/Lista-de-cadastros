81abc6ca1b3ba40e88dd53c2693da9f0a161ad07
Victor-Emanuell <166446187+Victor-Emanuell@users.noreply.github.com>
2025-09-05 21:12:37 -0300

    first commit

---
+++ src/main/java/com/EmpresaX/ListaDeCadastros/ListaDeCadastrosApplication.java
@@ -0,0 +1,13 @@
+package com.EmpresaX.ListaDeCadastros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeCadastrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeCadastrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmpresaX/ListaDeCadastros/ListaDeCadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmpresaX.ListaDeCadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeCadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
